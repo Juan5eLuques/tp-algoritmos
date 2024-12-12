ad0cd20cc0a088bbc9959829629eeddbc4d6a5c8
Juan5eLuques <luquesjuanse.10@gmail.com>
2024-12-12 17:16:29 -0300

    tablero fijo de 10x20

--- tp integrador algoritmos.cpp
+++ tp integrador algoritmos.cpp
@@ -308,7 +308,7 @@ int main(int argc, char *argv[]) {
 		
 		case 2:{ok5=false;
 		while(!ok5){
-			
+			system("cls");
 			cout << "Juego Super Mario Bros - Simulación X" << endl;
 			cout << "=====================================" << endl;
 			cout << endl;
@@ -773,10 +773,7 @@ void algoritmo3(){
 							char tablero[10][20];
 							char tablerosimulacion[10][20];
 							int filas,columnas;
-							cout << "Ingrese la cantidad de filas del tablero (como maximo pueden ser 10): ";
-							cin >> filas;
-							cout << endl <<"Ingrese la cantidad de columnas del tablero (como maximo pueden ser 20): ";
-							cin >> columnas;
+							filas=10, columnas =20;
 							cout << endl;
 							for(int i=0;i<filas;i++){
 								for(int j=0;j<columnas;j++){
